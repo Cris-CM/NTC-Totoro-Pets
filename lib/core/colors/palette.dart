6901c5c5da9ae84436a09d075f14e5c794a505bd
LALO611adad <166889169+Cris-CM@users.noreply.github.com>
2024-05-02 03:04:17 -0500

    feat: palette completado

--- lib/core/colors/palette.dart
+++ lib/core/colors/palette.dart
@@ -2,4 +2,11 @@ import 'package:flutter/material.dart';
 
 class Palette {
   static const black = Colors.black;
+  static const blueGray = Color(0xFFD4D4D4);
+  static const white = Color(0xFFFFFFFF);
+  static const green = Color(0xFF5CB15A);
+
+  static const yellow = Color(0xFFF0BB22);
+  static const purple = Color(0xFf7A86AE);
+  static const red = Color(0xFFE54D4D);
 }
--- lib/features/Auth/Presentation/login_page.dart
+++ lib/features/Auth/Presentation/login_page.dart
@@ -8,22 +8,54 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                context.go('/home');
-              },
-              style: ElevatedButton.styleFrom(backgroundColor: Palette.black),
-              child: const Text('hola'),
-            ),
-            Image.asset(
-              'assets/images/pikachu.png',
-            ),
-          ],
-        ),
+      backgroundColor: Palette.blueGray,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+              child: Column(
+            children: [
+              Image.asset(
+                'assets/images/pikachu.png',
+                height: 100,
+              ),
+              const Text(
+                'Hola Bienvenido',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 140),
+                child: ElevatedButton(
+                  onPressed: () {
+                    context.go('/home');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Palette.green,
+                  ),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Empezar',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                            color: Palette.white),
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: Palette.white,
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ))
+        ],
       ),
     );
   }
